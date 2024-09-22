a99bac934338af271505fbea8800f441e931f578
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-22 14:07:07 +0530

    Add files via upload

---
+++ area and perimeter of rectangle.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    int l = 20, b = 10;
+    printf("Area of rectangle is : %d", l * b);
+     printf("\nPerimeter of rectangle is : %d", 2 * (l + b));
+}
